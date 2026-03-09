d939850842c91b877314cf1fda9cb256604b9271
austin-casey <141893333+austin-casey@users.noreply.github.com>
2026-03-09 12:21:24 -0800

    Basic functions, layout, and styling

--- script.js
+++ script.js
@@ -22,3 +22,16 @@ const divide = function(num1, num2) {
 	const divided = num1 / num2; 
     return divided;
 };
+
+const operate = function(num1, operator, num2) {
+    switch(operator) {
+        case '+':
+            return add(num1, num2);
+        case '-': 
+            return subtract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+    }
+}
