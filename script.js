4e945abb47f861b9371106096301d315940e0859
austin-casey <141893333+austin-casey@users.noreply.github.com>
2026-03-08 16:30:23 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let num1 = null;
+let num2 = null;
+let operator = null;
+
+const add = function(num1, num2) {
+	const added = num1 + num2; 
+    return added
+}
+
+const subtract = function(num1, num2) {
+    const subtracted = num1 - num2;
+    return subtracted;
+
+}
+
+const multiply = function(num1, num2) {
+	const multiplied = num1 * num2; 
+    return multiplied;
+}
+
+const divide = function(num1, num2) {
+	const divided = num1 / num2; 
+    return divided;
+};
