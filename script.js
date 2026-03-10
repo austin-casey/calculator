a799ce0b617c70d6079e710c4706beb3282755ea
austin-casey <141893333+austin-casey@users.noreply.github.com>
2026-03-10 11:07:46 -0800

    Completed calculator project

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ let num2 = "";
 let operator = null;
 let result = "";
 
+//used to translate multiplication and division symbols
+const keyMap = {
+    '*': 'x',
+    '/': '÷'
+};
+
 const display = document.querySelector(".display");
 const container = document.querySelector(".container"); 
 
@@ -26,6 +32,14 @@ const divide = function(num1, num2) {
     return divided;
     }
 
+const clear = function () {
+        display.textContent = '';
+        num1 = "";
+        num2 = "";
+        operator = null;
+        result = "";
+}
+
 const operate = function(num1, operator, num2) {
     let number1 = Number(num1);
     let number2 = Number(num2);
@@ -37,10 +51,102 @@ const operate = function(num1, operator, num2) {
         case 'x':
             return multiply(number1, number2);
         case '÷':
-            return divide(number1, number2);
+            if(number2 === 0) {
+                alert("You can't divide by zero, dummy.")
+                return divide(number1, number2);
+            } else {
+                return divide(number1, number2);
+            }
     }
 }
 
+// adds keyboard functionality
+document.addEventListener('keydown', (e) => {
+    e.preventDefault();
+    if(/[0-9]/.test(e.key))
+        if(result !== '') {
+            num1 = "";
+            result = "";
+            num1 += e.key;
+            display.textContent = `${num1}`;
+        }else if(operator === null){
+            num1 += e.key;
+            display.textContent = `${num1}`;
+        } else if (operator !== null) {
+            num2 += e.key;
+            display.textContent = `${num1} ${operator} ${num2}`;
+        }
+
+    if(/[+\-*/]/.test(e.key)){
+        if(num1 !== "" && num2 === "") {
+            result = "";
+            operator = keyMap[e.key] || e.key;
+            display.textContent = `${num1} ${operator}`;
+        } else if(num1 !== "" && num2 !== "") {
+            let newOperator = keyMap[e.key] || e.key;
+            result = operate(num1, operator, num2);
+            num1 = parseFloat(result.toFixed(2));
+            num2 = "";
+            result = "";
+            operator = newOperator;
+            display.textContent = `${num1} ${operator}`;
+        }
+    }
+
+        if((e.key === "=" || e.key === "Enter") && 
+        num1 !== "" && 
+        num2 !== "" &&
+        operator !== null) {
+            result = operate(num1, operator, num2);
+            if(result === Infinity|| isNaN(result)){
+                clear();
+            } else {
+            num1 = parseFloat(result.toFixed(2));
+            num2 = '';
+            operator = null;
+            display.textContent = `${num1}`;
+            }
+        }
+
+        if(e.key === 'Backspace') {
+            if(num2 !== "") {
+                num2 = num2.slice(0, -1);
+                display.textContent = `${num1} ${operator} ${num2}`;
+            } else if(num2 === "" && operator !== null) {
+                operator = null;
+                display.textContent = `${num1}`;
+            } else {
+                num1 = num1.slice(0, -1)
+                display.textContent = `${num1}`;
+            }
+}
+
+    if(e.key === ".") {
+        if(result !== '') {
+            num1 = "";
+            result = "";
+            num1 +=e.key;
+            display.textContent = `${num1}`;
+        }else if(operator === null){
+            if(num1.includes('.')) {
+                alert('Your number can only include one decimal point')
+            } else {
+                num1 += e.key;
+                display.textContent = `${num1}`;
+            }
+        } else if (operator !== null) {
+            if(num2.includes('.')) {
+                alert('Your number can only include one decimal point')
+            } else {
+                num2 += e.key;
+                display.textContent = `${num1} ${operator} ${num2}`;
+            }
+        }
+        } 
+})
+
+
+// adds click functionality
 container.addEventListener('click', (e) => {
 
     if(e.target.classList.contains('digit')){
@@ -60,13 +166,15 @@ container.addEventListener('click', (e) => {
 
     if(e.target.classList.contains('operator')) {
         if(num1 !== "" && num2 === "") {
+            result = "";
             operator = e.target.textContent;
             display.textContent = `${num1} ${operator}`;
         } else if(num1 !== "" && num2 !== "") {
             let newOperator = e.target.textContent;
-            result = operate(num1, operator, num2)
+            result = operate(num1, operator, num2);
             num1 = parseFloat(result.toFixed(2));
             num2 = "";
+            result = "";
             operator = newOperator;
             display.textContent = `${num1} ${operator}`;
         }
@@ -77,16 +185,56 @@ container.addEventListener('click', (e) => {
         num2 !== "" &&
         operator !== null) {
             result = operate(num1, operator, num2);
+            if(result === Infinity|| isNaN(result)){
+                clear();
+            } else {
             num1 = parseFloat(result.toFixed(2));
             num2 = '';
             operator = null;
-            display.textContent = `${num1}`;
+            display.textContent = `${num1}`
+            }
         }
 
     if(e.target.classList.contains('clear')) {
-        display.textContent = '';
-        num1 = "";
-        num2 = "";
-        operator = null;
+        clear();
     }
+
+    if(e.target.classList.contains('backspace')) {
+        if(num2 !== "") {
+            num2 = num2.slice(0, -1);
+            display.textContent = `${num1} ${operator} ${num2}`;
+        } else if(num2 === "" && operator !== null) {
+            operator = null;
+            display.textContent = `${num1}`;
+        } else {
+            num1 = num1.slice(0, -1)
+            display.textContent = `${num1}`;
+        }
+    }
+
+    if(e.target.classList.contains('decimal')) {
+        if(result !== '') {
+            num1 = "";
+            result = "";
+            num1 +=e.target.textContent;
+            display.textContent = `${num1}`;
+        }else if(operator === null){
+            if(num1.includes('.')) {
+                alert('Your number can only include one decimal point')
+            } else {
+                num1 += e.target.textContent;
+                display.textContent = `${num1}`;
+            }
+        } else if (operator !== null) {
+            if(num2.includes('.')) {
+                alert('Your number can only include one decimal point')
+            } else {
+                num2 += e.target.textContent;
+                display.textContent = `${num1} ${operator} ${num2}`;
+            }
+        }
+        } 
+     if(e.target.classList.contains('show')){
+        alert("This button is just for show. It doesn't work.")
+     }
 })
